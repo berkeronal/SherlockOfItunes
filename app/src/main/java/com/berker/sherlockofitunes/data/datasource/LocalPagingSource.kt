8da62b38072dba354a87088a7e44f1721e5a04b7
Berker <berker435@gmail.com>
2022-06-12 02:53:22 +0300

    LocalPagingSource added

---
+++ app/src/main/java/com/berker/sherlockofitunes/data/datasource/LocalPagingSource.kt
@@ -0,0 +1,41 @@
+package com.berker.sherlockofitunes.data.datasource
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+
+class LocalPagingSource<T : Any>(
+    private val block: suspend (Int) -> List<T>?
+) : PagingSource<Int, T>() {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, T> {
+        val position = params.key ?: STARTING_INDEX
+        val offset = (position - 1) * STARTING_INDEX
+
+        return try {
+            val result = block.invoke(offset)
+
+            result?.let {
+                LoadResult.Page(
+                    data = it,
+                    prevKey = if (position == STARTING_INDEX) 0 else position.minus(1),
+                    nextKey = position.plus(1)
+                )
+            }?: run{
+                LoadResult.Error(Exception("Error"))
+            }
+
+        } catch (exception: Exception) {
+            LoadResult.Error(exception)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, T>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+    }
+
+    companion object {
+        private const val STARTING_INDEX = 0
+    }
+}
