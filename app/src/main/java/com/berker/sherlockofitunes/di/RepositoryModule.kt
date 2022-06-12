e33b626022d17f3b5711e6f756f5393d5d94b502
Berker <berker435@gmail.com>
2022-06-12 03:54:16 +0300

    Repository Module added

--- app/src/main/java/com/berker/sherlockofitunes/di/RepositoryModule.kt
+++ app/src/main/java/com/berker/sherlockofitunes/di/RepositoryModule.kt
@@ -1,4 +1,24 @@
 package com.berker.sherlockofitunes.di
 
+import com.berker.sherlockofitunes.data.remote.ITunesContentApi
+import com.berker.sherlockofitunes.data.repository.ITunesContentRepositoryImpl
+import com.berker.sherlockofitunes.domain.repository.ITunesContentRepository
+import com.berker.sherlockofitunes.mapper.DomainMapper
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 class RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideITunesContentRepository(
+        api: ITunesContentApi,
+        domainMapper: DomainMapper
+    ): ITunesContentRepository = ITunesContentRepositoryImpl(api = api, domainMapper = domainMapper)
+
 }
