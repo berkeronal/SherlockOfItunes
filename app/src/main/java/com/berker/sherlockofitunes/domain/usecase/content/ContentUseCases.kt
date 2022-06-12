a8509d9e087c02c1ed37b426bd7c3201207333b1
Berker <berker435@gmail.com>
2022-06-12 03:13:04 +0300

    ContentUseCases added

---
+++ app/src/main/java/com/berker/sherlockofitunes/domain/usecase/content/ContentUseCases.kt
@@ -0,0 +1,5 @@
+package com.berker.sherlockofitunes.domain.usecase.content
+
+data class ContentUseCases(
+    val getContentWithPaging: GetContentWithPaging
+)
---
+++ app/src/main/java/com/berker/sherlockofitunes/domain/usecase/content/GetContentWithPaging.kt
@@ -0,0 +1,16 @@
+package com.berker.sherlockofitunes.domain.usecase.content
+
+import androidx.paging.PagingData
+import com.berker.sherlockofitunes.domain.model.Content
+import com.berker.sherlockofitunes.domain.repository.ITunesContentRepository
+import kotlinx.coroutines.flow.Flow
+
+class GetContentWithPaging(
+    private val repository: ITunesContentRepository
+) {
+
+    operator fun invoke(): Flow<PagingData<Content>> = repository.getContentsByTerm(
+        "BERKER",
+        "music"
+    )
+}
