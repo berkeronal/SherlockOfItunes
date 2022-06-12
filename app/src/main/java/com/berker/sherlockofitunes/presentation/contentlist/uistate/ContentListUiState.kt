8896efebb800ce033072e62696547c304805e5e9
Berker <berker435@gmail.com>
2022-06-13 01:20:51 +0300

    ContentListUiState default values added for initialize

--- app/src/main/java/com/berker/sherlockofitunes/presentation/contentlist/uistate/ContentListUiState.kt
+++ app/src/main/java/com/berker/sherlockofitunes/presentation/contentlist/uistate/ContentListUiState.kt
@@ -1,12 +1,15 @@
 package com.berker.sherlockofitunes.presentation.contentlist.uistate
 
+import androidx.paging.LoadState
 import androidx.paging.PagingData
 import com.berker.sherlockofitunes.core.BaseUiState
 import com.berker.sherlockofitunes.domain.model.ContentType
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
 
 data class ContentListUiState(
-    val term: String,
-    val contentType: ContentType,
-    val contents: Flow<PagingData<ContentItemUiState>>
+    val term: String = "",
+    val contentType: ContentType = ContentType.Movie,
+    val contents: Flow<PagingData<ContentItemUiState>> = emptyFlow(),
+    val loadState: LoadState = LoadState.Loading,
 ) : BaseUiState()
